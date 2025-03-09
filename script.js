935c21009407e83205a596c1925b4d076eae9f17
ashwin <ashwinharikaran2006@gmail.com>
2025-03-09 20:13:38 +0530

    added new command

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function toggleMenu() {
     const nav = document.querySelector('.nav-links');
     nav.classList.toggle('active');
 }
+
+// Select elements
+const sizeSelect = document.getElementById('size');
+const toppings = document.querySelectorAll('.toppings input');
+const totalPriceDisplay = document.getElementById('totalPrice');
+
+// Function to update price
+function updatePrice() {
+    let totalPrice
+}
