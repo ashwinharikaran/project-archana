4ccc64d6643658d73aa4f24f59686688748b5cb2
ashwin <ashwinharikaran2006@gmail.com>
2025-03-09 20:29:03 +0530

    added new command

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Select elements
+const sizeSelect = document.getElementById('size');
+const toppings = document.querySelectorAll('.toppings input');
+const totalPriceDisplay = document.getElementById('totalPrice');
+
+// Function to update price
+function updatePrice() {
+    let totalPrice
+
 
 // Select elements
 const sizeSelect = document.getElementById('size');
