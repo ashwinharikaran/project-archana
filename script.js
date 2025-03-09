9701561ae2355b9a00a6738d5c1d274f02af5fed
ashwin <ashwinharikaran2006@gmail.com>
2025-03-09 12:00:48 +0530

    added new command

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const orderButtons = document.querySelectorAll(".order-btn");
-    
-    orderButtons.forEach(button => {
-        button.addEventListener("click", function () {
-            alert("Item added to cart!");
-        });
-    });
-});
